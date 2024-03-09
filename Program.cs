e76c4643ca2da762ccbebe2ea323b2144c6463fc
NothingMeMe <113574573+UwUhub@users.noreply.github.com>
2024-03-09 19:08:56 +0600

    фцвфцвцфв

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Popravka 1 Amantur");
+Console.WriteLine("Popravka 2 Amantur"); 
